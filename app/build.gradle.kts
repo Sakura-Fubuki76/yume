205789c13da7df6bdcf0b9e679394696756d26f5
SakuraFubuki <3210929860@qq.com>
2026-05-17 21:15:17 +0800

    chore: bump version to 1.0.0-alpha01 and optimize publish workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         applicationId = "com.sakurafubuki.yume"
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0-alpha01"
     }
 
     buildFeatures {
